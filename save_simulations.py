8f7c2f3bbf4aff745df88a2d9826df31214e10a3
brownpr <brownpr10@gmail.com>
2021-08-06 18:04:05 +0200

    Script for saving evoliutionary history

---
+++ save_simulations.py
@@ -0,0 +1,18 @@
+import datetime
+import sys
+
+from population_async import Population
+import os
+
+if __name__ == "__main__":
+    try:
+        # Create file to save creature files, if file exists stop sim
+        if not os.path.exists("generated_files"):
+            raise Exception("STOPPING SIMULATION: Creature files does not exist. Please run initialize.py before running"
+                            " continue.py")
+
+        loaded_population = Population(load_population=True)
+        loaded_population.save_evolutionary_history()
+
+    except KeyboardInterrupt:
+        sys.exit()
