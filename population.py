9ef2bac749ef99c58f0d9ee0100f4b17bab006dd
brownpr <brownpr10@gmail.com>
2021-08-06 18:03:09 +0200

    Implemented feature to be able to load and save population.

--- population.py
+++ population.py
@@ -1,6 +1,8 @@
 import json
 import os
 import datetime as dt
+import sys
+import pickle
 import operator
 import subprocess as sub
 import time
@@ -11,37 +13,68 @@ from creature import Creature
 
 
 class Population:
-    def __init__(self, population=None, damaged_population=False, reset_evolution=False):
+    def __init__(self, population=None, is_damaged=False, reset_evolution=False, load_population=False):
         # Import settings
         settings_file = open("settings.json")
         self.settings = json.load(settings_file)
         settings_file.close()
 
-        # When called for first time, creates a new population of creatures.
-        self.population = {}                            # dict, Evaluation population
-        self.full_population = {}                       # dict, Full population
-        self.damaged_population = damaged_population    # bool, has population been damaged?
-        self.damage_type = None                         # string, what type of damage has been inflicted on creature
-        self.damage_arguments = None                    # Previously used arguments (all creatures created in a damaged
-        #                                                   population will receive the same damage as the originals)
+        if not load_population:
 
-        # Create an example creature to copy from (as many parameters don't change when creating an additional creature)
-        self.base_creature = Creature(name="base_creature")
-        self.base_creature.set_neighbours_and_sections()
+            # When called for first time, creates a new population of creatures.
+            self.population = {}                            # dict, Evaluation population
+            self.full_population = {}                       # dict, Full population
+            self.is_damaged = is_damaged    # bool, has population been damaged?
+            self.damage_type = None                         # string, what type of damage has been inflicted on creature
+            self.damage_arguments = None                    # Previously used arguments (all creatures created in a damaged
+            #                                                   population will receive the same damage as the originals)
 
-        # If population introduced into system add to self.population, else create new starting population
-        if population is not None:
-            self.population = population
+            # Create an example creature to copy from (as many parameters don't change when creating an additional creature)
+            self.base_creature = Creature(name="base_creature")
+            self.base_creature.set_neighbours_and_sections()
+
+            # If population introduced into system add to self.population, else create new starting population
+            if population is not None:
+                self.population = population
+            else:
+                self.create_new_population((0, self.settings["parameters"]["pop_size"]))
+
+            # Set full population
+            self.full_population = self.population          # Full population is initial population
+
+            # Set parameter to remember last evaluation
+            self.last_generation = 0
+
         else:
-            self.create_new_population((0, self.settings["parameters"]["pop_size"]))
+            loaded_population = self.load_population()                      # Load population
+
+            # Set parameters from loaded population to this population
+            if population is not None:
+                self.population = population
+            else:
+                self.population = loaded_population.population              # dict, Evaluation population
+            self.full_population = loaded_population.full_population        # dict, Full population
+            self.is_damaged = loaded_population.is_damaged  # bool, has population been damaged?
+
+            if self.is_damaged != is_damaged:
+                raise Exception("You are loading a population which does not share the same damage type."
+                                "The population you are creating has damage set to: " + str(is_damaged) +
+                                " The population you are loading has damage set to: " + str(self.is_damaged) +
+                                " Please make sure you are creating a population with the same damage type as the " \
+                                "population you are creating.")
+
+            self.damage_type = loaded_population.damage_type                # string, what type of damage has been inflicted on creature
+            self.damage_arguments = loaded_population.damage_arguments      # Previously used arguments (all creatures created in a damaged
+            #                                                               population will receive the same damage as the originals)
+
+            self.base_creature = loaded_population.base_creature            # Example creature
+
+            self.last_generation = loaded_population.last_generation
 
         # If reset_evolution, reset evolutionary history
         if reset_evolution:
             for creature in self.population.values():
                 creature.evolution = {}
-
-        # Full population is initial population
-        self.full_population = self.population                      # dall creatures created in population
 
     def create_new_population(self, population_range):
         # ARGUMENTS:
@@ -68,7 +101,7 @@ class Population:
             self.full_population[creature.name] = creature
 
         # If the creature to be added to a damaged population class, damage said creatures before adding them
-        if self.damaged_population:
+        if self.is_damaged:
             self.population = self.inflict_damage(self.damage_type, self.damage_arguments)
 
     def run_genetic_algorithm(self, generation_size=None):
@@ -79,15 +112,20 @@ class Population:
             generation_size = self.settings["parameters"]["gen_size"]
 
         # Print Creature Genomes
-        if self.damaged_population:
+        if self.is_damaged:
             print(str(dt.datetime.now()) + " INITIAL DAMAGED POPULATION: ")
         else:
             print(str(dt.datetime.now()) + " INITIAL UNDAMAGED POPULATION: ")
 
         print([str(creature.name) for creature in self.population.values()])
 
+        if self.last_generation == 0:
+            rng = (0, generation_size)
+        else:
+            rng = (self.last_generation + 1, self.last_generation + 1 + generation_size)
+
         # Initialize genetic algorithm
-        for generation_num in range(generation_size):
+        for generation_num in range(rng[0], rng[1]):
             # Provide user with generation number
             print(str(dt.datetime.now()) + " CURRENT GENERATION NUMBER: " + str(generation_num))
             # Print list of creatures under evaluation
@@ -107,7 +145,9 @@ class Population:
             print(str(dt.datetime.now()) + " Finished evaluating population, top performing creature:"
                   + top_creature.name + ". Fitness: " + str(top_creature.fitness_eval))
 
-        if self.damaged_population:
+            self.last_generation = generation_num
+
+        if self.is_damaged:
             print(str(dt.datetime.now()) + " FINISHED SIMULATIONS FOR " + self.damage_type.upper() +
                   " DAMAGED CREATURES.")
         else:
@@ -115,7 +155,7 @@ class Population:
 
     def evaluate_population(self, generation_number):
         # ARGUMENTS
-        # - generation_num:                                             int, Current generation number
+        # - generation_num:        int, Current generation
 
         # Working directories variables
         cwd = os.getcwd()
@@ -149,9 +189,10 @@ class Population:
                 while not os.path.exists(pfp) or not os.path.exists(ffp):
                     time.sleep(1)
                     toc = time.time() - t
-                    if toc > 120:
-                        raise Exception("ERROR: No pressure file or fitness file found after 120 seconds. "
-                                        "This error is commonly due to problems in the created vxa file.")
+                    if toc > 300:
+                        raise Exception("ERROR: No pressure file or fitness file for " + creature.name +
+                                        "found after 300 seconds. This error is commonly due to problems in the created"
+                                        " vxa file.")
 
                 # Update creature fitness
                 creature.calculate_fitness()
@@ -195,6 +236,19 @@ class Population:
                 # If at last episode, reset morphology and stiffness
                 if episode == self.settings["parameters"]["ep_size"] - 1:
                     creature.reset_morphology()
+
+    def save_population(self):
+        sys.setrecursionlimit(10000)
+
+        with open("previous_population.pkl", "wb") as w_file:
+            pickle.dump(self, w_file)
+        w_file.close()
+
+    def load_population(self):
+        with open("previous_population.pkl", "rb") as file:
+            loaded_pop = pickle.load(file)
+        file.close()
+        return loaded_pop
 
     def new_population(self):
         # Function sorts previously evaluated population and selects top performers, evolves the neural network of a
@@ -252,7 +306,7 @@ class Population:
 
         return sorted_population, top_creature
 
-    def save_population(self, population=None):
+    def save_evolutionary_history(self, population=None):
         # Save data of the top preforming creatures throughout generations. Run at the end of evaluation
         # If pop not specified used self.population
         if population is None:
@@ -262,7 +316,7 @@ class Population:
         sorted_pop, top_creature = self.sort_population(population)
 
         # Set file name dependant on damage
-        if self.damaged_population:
+        if self.is_damaged:
             file_name = "damaged_evolution_" + self.damage_type
         else:
             file_name = "undamaged_evolution"
@@ -276,7 +330,7 @@ class Population:
         population_file.close()
 
         # Save evolutionary history of creatures
-        for creature in self.population.values():
+        for creature in population.values():
             with open("generated_files/" + creature.name + "/evolution.json", "w") as creature_file:
                 json.dump(creature.evolution, creature_file, sort_keys=True, indent=4)
             creature_file.close()
