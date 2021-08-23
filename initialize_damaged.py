035c587466416c5f2e2b3cdb0f69204c011d1bbc
brownpr <71068591+brownpr@users.noreply.github.com>
2021-08-23 18:08:22 +0200

    Update initialize_damaged.py

--- initialize_damaged.py
+++ initialize_damaged.py
@@ -4,14 +4,39 @@ from population_async import Population
 
 if __name__ == "__main__":
     try:
+        # Create file to save creature files, if file exists stop sim
+        if not os.path.exists("generated_files"):
+            os.mkdir("generated_files")
+        else:
+            raise Exception("STOPPING SIMULATION: Creature files may already exist. Please delete or "
+                  "rename the 'generated_files' folder.")
+
+
         # Damage population
         undamaged_population = Population(load_population=True)
-        num_creatures_to_damage = 20
-        undamaged_population_sorted, top_creature = undamaged_population.sort_population(undamaged_population
-                                                                                         .full_population)
+        num_creatures_to_damage = 50
+        undamaged_population_sorted, _ = undamaged_population.sort_population(undamaged_population.full_population)
         damaged_population_dict = {crt.name: crt for crt in undamaged_population_sorted[0:num_creatures_to_damage]}
 
         damaged_population = Population(population=damaged_population_dict, is_damaged=True, reset_evolution=True)
+
+        # Inflict damage on each creature (choose damage type)
+        damaged_population.inflict_damage(damage_type="remove_sect", damage_arguments=[(1, 3, 5, 7)],
+                                          damage_base_creature=True)
+        # damaged_population.inflict_damage(damage_type="stiff_spher_red", damage_arguments=[(0, 0, 0), 3, 200000],
+        #                                   damage_base_creature=True)
+
+        # Evaluate creatures for n evaluations
+        damaged_population.run_genetic_algorithm()
+
+        # Save population
+        # damaged_population.save_evolutionary_history()
+
+        print(str(datetime.datetime.now()) + "-----FINISHED EVALUATION-----")
+
+    except KeyboardInterrupt:
+        exit()
+
 
         # List of damage types and their arguments:
         #
@@ -33,19 +58,3 @@ if __name__ == "__main__":
         # stiff_spher_add           3 -- tuple, int, float -- centre voxel coordinates, affected radius, increase stiffness by input
         # stiff_spher_red           3 -- tuple, int, float -- centre voxel coordinates, affected radius, reduce stiffness by input
         #
-
-        # Inflict damage on each creature (choose damage type)
-        damaged_population.inflict_damage(damage_type="remove_sect", damage_arguments=[(1, 3)], damage_base_creature=True)
-        # damaged_population.inflict_damage(damage_type="stiff_spher_red", damage_arguments=[(0, 0, 0), 3, 200000],
-        #                                   damage_base_creature=True)
-
-        # Evaluate creatures for n evaluations
-        damaged_population.run_genetic_algorithm()
-
-        # Save population
-        # damaged_population.save_evolutionary_history()
-
-        print(str(datetime.datetime.now()) + "-----FINISHED EVALUATION-----")
-
-    except KeyboardInterrupt:
-        exit()
