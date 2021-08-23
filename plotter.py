b1f9edeba31f227ae31b3487017927b80598fc63
brownpr <71068591+brownpr@users.noreply.github.com>
2021-08-23 18:11:48 +0200

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -6,134 +6,151 @@ UNDAMAGED_PERFORMANCE_FILE = 'performance_undamaged_evolution.json'
 DAMAGED_PERFORMANCE_FILE = 'performance_damaged_evolution_remove_sect.json'
 CREATURE_FILE = "evolution.json"
 LAST_EPISODE_NUMBER = "14"
+cwd = os.getcwd()
+DATA_FOLDER_PATH = os.path.join(cwd, "generated_files")
 
+def import_top_performers():
 
-def creature_plots(data_folder_fp):
-
-    performances = {}
-    neural_networks = {}
-    morphological_evolution = {}
-    stiffness_evolution = {}
-
-    for subdir, dirs, files in os.walk(data_folder_fp):
-
-        if UNDAMAGED_PERFORMANCE_FILE in files:
-            undamaged_creatures_file = open(os.path.join(subdir, UNDAMAGED_PERFORMANCE_FILE))
-            undamaged_creatures_data = json.load(undamaged_creatures_file)
-            undamaged_creatures_file.close()
-
-            # turn list of dictionaries into dictionary
-            y_values = []
-            x_labels = []
-            for item in undamaged_creatures_data:
-                y_values.append(list(item.values())[0])
-                x_labels.append(list(item.keys())[0])
-
-            x_labels.reverse()
-            y_values.reverse()
-            x_values = range(len(undamaged_creatures_data))
-
-            plt.plot(x_values, y_values)
-            plt.xticks(x_values, x_labels, rotation=70)
-            plt.show()
-
-        if DAMAGED_PERFORMANCE_FILE in files:
-            damaged_creatures_file = open(os.path.join(subdir, DAMAGED_PERFORMANCE_FILE))
-            damaged_creatures_data = json.load(damaged_creatures_file)
-            damaged_creatures_file.close()
-
-            # turn list of dictionaries into dictionary
-            y_values = []
-            x_labels = []
-            for item in damaged_creatures_data:
-                y_values.append(list(item.values())[0])
-                x_labels.append(list(item.keys())[0])
-
-            x_labels.reverse()
-            y_values.reverse()
-            x_values = range(len(damaged_creatures_data))
-
-            plt.plot(x_values, y_values)
-            plt.xticks(x_values, x_labels, rotation=70)
-            plt.show()
-
+    generations = {}
+    # Data extraction
+    for subdir, dirs, files in os.walk(DATA_FOLDER_PATH):
         if CREATURE_FILE in files:
             creatures_file = open(os.path.join(subdir, CREATURE_FILE))
             creature_data = json.load(creatures_file)
             creatures_file.close()
 
-            creature_name = creatures_file.name.replace(CREATURE_FILE, "").replace(data_folder_fp, "").replace("\\", "")
+            creature_name = creatures_file.name.replace(CREATURE_FILE, "").replace(DATA_FOLDER_PATH, "") \
+                .replace("\\", "").replace("/", "")
 
-            if creature_name not in neural_networks:
-                neural_networks.update({creature_name: {}})
-            if creature_name not in morphological_evolution:
-                morphological_evolution.update({creature_name: {}})
-            if creature_name not in stiffness_evolution:
-                stiffness_evolution.update({creature_name: {}})
+            for gen in creature_data:
 
-            for generation in creature_data.keys():
-
-                if len(generation) == 5:
-                    gen = generation.replace("_", "_0")
+                # rename generations
+                if len(gen) == 5:  # gen number under 10
+                    generation = gen.replace("_", "_00")
+                elif len(gen) == 6:  # between gen number 10 and 99
+                    generation = gen.replace("_", "_0")
+                # If gen number exceeds 999 add a zero to the above replaces and uncomment this bellow
+                # elif len(gen) == 7:  # between gen number 100 and 999
+                #     generation = gen.replace("_", "_0")
                 else:
-                    gen = generation
+                    generation = gen
 
-                if gen not in morphological_evolution[creature_name]:
-                    morphological_evolution[creature_name].update({gen: {}})
+                # get creature performance
+                creature_performance = creature_data[gen]["ep_" + LAST_EPISODE_NUMBER]["fitness_eval"]
 
-                if gen not in stiffness_evolution[creature_name]:
-                    stiffness_evolution[creature_name].update({gen: {}})
+                # If no section for generation, create one
+                if generation not in generations:
+                    # As this data is the only one, give it default values for this creature
+                    generations.update({generation: {"max": [creature_performance, creature_name],
+                                                     "min": [creature_performance, creature_name]}})
 
-                for episode in creature_data[generation].keys():
-                    if episode == "nn_parameters":
-                        neural_networks[creature_name].update({gen: creature_data[generation]["nn_parameters"]})
-                        break
-                    elif len(episode) == 4:
-                        ep = episode.replace("_", "_0")
-                    else:
-                        ep = episode
+                else:
+                    if generations[generation]["max"][0] < creature_performance:
+                        generations[generation]["max"][0] = creature_performance
+                        generations[generation]["max"][1] = creature_name
 
-                    if gen not in performances:
-                        performances.update({gen: {
-                            "max": creature_data[generation][episode]["fitness_eval"],
-                            "min": creature_data[generation][episode]["fitness_eval"]}})
+                    if generations[generation]["min"][0] > creature_performance:
+                        generations[generation]["min"][0] = creature_performance
+                        generations[generation]["min"][1] = creature_name
 
-                    if creature_data[generation][episode]["fitness_eval"] > performances[gen]["max"]:
-                        performances[gen]["max"] = creature_data[generation][episode]["fitness_eval"]
+    return generations
 
-                    if creature_data[generation][episode]["fitness_eval"] < performances[gen]["min"]:
-                        performances[gen]["min"] = creature_data[generation][episode]["fitness_eval"]
 
-                    if ep not in morphological_evolution[creature_name][gen]:
-                        morphological_evolution[creature_name][gen].update({
-                            ep: creature_data[generation][episode]["morphology"]})
+def import_creature_performance(creature_name):
 
-                    if ep not in stiffness_evolution[creature_name][gen]:
-                        stiffness_evolution[creature_name][gen].update({
-                            ep: creature_data[generation][episode]["stiffness"]})
+    creature_performances = {}
+    for subdir, dirs, files in os.walk(os.path.join(DATA_FOLDER_PATH, creature_name)):
+        if CREATURE_FILE in files:
+            creatures_file = open(os.path.join(subdir, CREATURE_FILE))
+            creature_data = json.load(creatures_file)
+            creatures_file.close()
+
+            for gen in creature_data:
+
+                # rename generations
+                if len(gen) == 5:  # gen number under 10
+                    generation = gen.replace("_", "_00")
+                elif len(gen) == 6:  # between gen number 10 and 99
+                    generation = gen.replace("_", "_0")
+                # If gen number exceeds 999 add a zero to the above replaces and uncomment this bellow
+                # elif len(gen) == 7:  # between gen number 100 and 999
+                #     generation = gen.replace("_", "_0")
+                else:
+                    generation = gen
+
+                # get creature performance
+                creature_performance = creature_data[gen]["ep_" + LAST_EPISODE_NUMBER]["fitness_eval"]
+
+                # If no section for generation, create one
+                if generation not in creature_performances:
+                    # As this data is the only one, give it default values for this creature
+                    creature_performances.update({generation: creature_performance})
+
+    return creature_performances
+
+
+def plot_performance(data):
 
     performance_min_values = []
     performance_max_values = []
-    performance_x_values = []
+    performance_act_values = []
+    sorted_generations = sorted(data.keys())
+    
+    max_performance = None
+    for generation in sorted_generations:
+        performance_act_values.append(data[generation]["max"][0])
+        performance_min_values.append(data[generation]["min"][0])
+        
+        if max_performance is None:
+            max_performance = data[generation]["max"][0]
+        elif max_performance < data[generation]["max"][0]:
+            max_performance = data[generation]["max"][0]
 
-    sorted_performances = {}
-    for i in sorted(performances):
-        sorted_performances[i] = performances[i]
-    for generation, performance in sorted_performances.items():
-        performance_x_values.append(generation)
-        performance_max_values.append(performance["max"])
-        performance_min_values.append(performance["min"])
+        performance_max_values.append(max_performance)
 
-    plt.plot(performance_x_values, performance_max_values, label="max_performance")
-    plt.plot(performance_x_values, performance_min_values, label="min_performance")
-    plt.xticks(performance_x_values, performance_x_values, rotation=70)
+    plt.plot(sorted_generations, performance_max_values, label="max_performance")
+    plt.plot(sorted_generations, performance_act_values, label="actual_max_performance")
+    plt.plot(sorted_generations, performance_min_values, label="min_performance")
+    plt.xticks(sorted_generations[::25], sorted_generations[::25], rotation=70)
     plt.grid(color="0.95")
     plt.legend()
     plt.show()
+    pass
+
+
+def plot_top_performers(data):
+
+    top_performers = []
+    sorted_generations = sorted(data.keys())
+
+    for generation in sorted_generations:
+        top_performer = data[generation]["max"][1]
+
+        if top_performer not in top_performers:
+            top_performers.append(top_performer)
+
+    for creature in top_performers:
+        creature_data = import_creature_performance(creature)
+
+        creature_fitness = []
+
+        for generation in sorted_generations:
+            if generation not in creature_data:
+                creature_fitness.append(None)
+            else:
+                creature_fitness.append(creature_data[generation])
+
+        plt.plot(sorted_generations, creature_fitness, label=creature.replace("_", ""))
+
+    plt.xticks(sorted_generations[::25], sorted_generations[::25], rotation=70)
+    plt.grid(color="0.95")
+    plt.legend()
+    plt.show()
+    pass
 
 
 if __name__ == "__main__":
-    cwd = os.getcwd()
-    folder_path = os.path.join(cwd, "generated_files")
+    performance_data = import_top_performers()
 
-    creature_plots(folder_path)
+    # plot_performance(performance_data)
+    plot_top_performers(performance_data)
