e97f7777801e3630b9d343df28d8120acba01dc7
brownpr <brownpr10@gmail.com>
2021-09-12 20:46:01 +0200

    Updated

--- save_simulations.py
+++ save_simulations.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     try:
         # Create file to save creature files, if file exists stop sim
         if not os.path.exists("generated_files"):
-            raise Exception("STOPPING SIMULATION: Creature files does not exist. Please run initialize.py before"
-                            " running continue.py")
+            raise Exception("STOPPING SIMULATION: The 'generated_files' folder does not exist. Please copy folder into "
+                            "cwd or initialize a simulation before running save_simulations.py")
 
         loaded_population = Population(load_population=True)
         loaded_population.save_evolutionary_history()
